99b6f0b7d782a66d9d97377fb9eb6b7d5aa83ebd
ilan <ilan19555@gmail.com>
2020-12-15 17:30:14 +0200

    changed echo added methods

--- GoNature Server/src/SqlConnector/sqlConnector.java
+++ GoNature Server/src/SqlConnector/sqlConnector.java
@@ -1,10 +1,16 @@
 package SqlConnector;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 
 public class sqlConnector {
 
@@ -118,7 +124,109 @@ public class sqlConnector {
 	} catch (SQLException e) {
 		e.printStackTrace();
 	}
-	i= i/9;
-	return i++;
+	
+	return i;
 }
+	/*
+	 * will check the amount of orders that is in the system
+	 * 
+	 */
+	public int nextOrder() {
+		Statement stm;
+		int i=0;
+	try {
+		PreparedStatement ps = conn.prepareStatement("SELECT * FROM project.order");
+		stm = conn.createStatement();
+		ResultSet rs = ps.executeQuery();
+
+		while (rs.next()) {
+			i++;
+		}
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+	return i;
+	}
+	
+	/*
+	 * 
+	 * check how many visitors in total there is for the desired time and date and return this value
+	 * 
+	 */
+	public int howManyForCurrentTimeAndDat1e(String[] msg) {
+		String res;
+		int counter=0;
+		Statement stm;
+		try {
+			PreparedStatement ps = conn.prepareStatement("SELECT numOfVisitors FROM project.order WHERE TimeInPark=? AND DateOfVisit=? AND wantedPark=?");
+			ps.setString(1, msg[0]);
+			ps.setString(2, msg[1]);
+			ps.setString(3, msg[2]);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				res=rs.getString(1);
+				counter+=Integer.parseInt(res);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return counter;
+		
+	}
+	
+	/*
+	 * method that will check how many visitors allowed to be in wanted park in total.
+	 */
+	public int howManyAllowedInPark(String parkName) {
+		String res;
+		int counter=0;
+		Statement stm;
+		try {
+			PreparedStatement ps = conn.prepareStatement("SELECT maxAvailableVisitors FROM gonaturedb.park WHERE ParkName=?");
+			ps.setString(1, parkName);
+			
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			
+			res=rs.getString(1);
+			counter+=Integer.parseInt(res);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return counter;
+	}
+	
+	
+
+	public int howManyForCurrentTimeAndDate(String[] result) throws ParseException {
+		Statement stm;
+		LocalDate wanted1 = LocalDate.parse(result[3]);
+		Date wanted=java.sql.Date.valueOf(wanted1);
+		
+		int counter=0;
+		try {
+			PreparedStatement ps = conn.prepareStatement("SELECT numOfVisitors,orderNum FROM gonaturedb.order WHERE wantedPark=? AND DateOfVisit=? AND TimeInPark BETWEEN ? AND ? ");
+			ps.setString(1, result[2]);
+			ps.setString(3, result[0]);
+			ps.setString(4, result[1]);
+			ps.setDate(2, wanted);
+			ResultSet rs=ps.executeQuery();
+			
+			while(rs.next()) {
+				counter+=rs.getInt(1);
+				System.out.println(rs.getString(2));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return counter;
+	}
+	
 }
